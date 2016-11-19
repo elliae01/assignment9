efbc9275ae5cbd1156ea862ed923a3bc3c97b241
Josh Hoffman <hoffja08@students.ipfw.edu>
2016-11-19 12:21:27 -0500

    Bmi test methods (actual method contents)

--- assignment9/test/test/BmiCalculatorTester.java
+++ assignment9/test/test/BmiCalculatorTester.java
@@ -9,4 +9,26 @@ public class BmiCalculatorTester {
 
 	@Test
 	public void testCalculateBmi_A() {
+		BmiCalculator bmi = new BmiCalculator();
+		assertEquals(bmi.calculateBmi(1, 1, false), 10000, 0.1);
+	}
+
+	@Test
+	public void testCalculateBmi_B() {
+		BmiCalculator bmi = new BmiCalculator();
+		assertEquals(bmi.calculateBmi(999999, 999999, false), 0.01 , 0.001);
+	}
+
+	@Test
+	public void testCalculateBmi_C() {
+		BmiCalculator bmi = new BmiCalculator();
+		assertEquals(bmi.calculateBmi(1, 1, true), 703.07, 0.001);
+	}
+
+	@Test
+	public void testCalculateBmi_D() {
+		BmiCalculator bmi = new BmiCalculator();
+		assertEquals(bmi.calculateBmi(999999, 999999, true), 0, 0.1);
+	}
+
 }
