938f5639ff78fdf70158fc9bb46bc7baf8198d0f
Josh Hoffman <hoffja08@students.ipfw.edu>
2016-11-19 12:20:47 -0500

    Bmi test methods

---
+++ assignment9/test/test/BmiCalculatorTester.java
@@ -0,0 +1,12 @@
+package test;
+
+import static org.junit.Assert.*;
+import assignment9.BmiCalculator;
+
+import org.junit.Test;
+
+public class BmiCalculatorTester {
+
+	@Test
+	public void testCalculateBmi_A() {
+}
