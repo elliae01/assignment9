f6456e8f220f1d2988afe911964f26c0659433ce
Sam <Sam@192.168.3.114>
2016-11-19 20:50:10 -0500

    PieChart Stuff

---
+++ assignment9/src/assignment9/PiechartWebServiceManager.java
@@ -0,0 +1,81 @@
+package assignment9;
+
+import de.toolforge.googlechartwrapper.Color;
+import de.toolforge.googlechartwrapper.Dimension;
+import de.toolforge.googlechartwrapper.PieChart;
+import de.toolforge.googlechartwrapper.color.ChartColor;
+import de.toolforge.googlechartwrapper.data.PieChartSlice;
+
+enum CHART_TYPE
+{
+	totalCholesterol,
+	BMI,
+	bloodPressure
+}
+
+public class PiechartWebServiceManager 
+{
+	private final int CHART_COUNT = 3;
+	
+	private Dimension chartDimensions = new Dimension(500, 250);
+	
+	private PieChart pieCharts[] = new PieChart[CHART_COUNT];
+	
+	public PiechartWebServiceManager()
+	{
+		for (int index = 0; index < CHART_COUNT; index++)
+		{
+			pieCharts[index] = new PieChart(chartDimensions);
+		}
+		
+		pieCharts[0].addChartColor(new ChartColor(new Color(240, 50, 200)));
+		pieCharts[1].addChartColor(new ChartColor(new Color(50, 50, 240)));
+		pieCharts[2].addChartColor(new ChartColor(new Color(185, 0, 15)));
+	}
+	
+	public void addPieSlice(CHART_TYPE type, String label, int value)
+	{
+		switch (type)
+		{
+		case totalCholesterol:
+			pieCharts[0].addPieChartSlice(new PieChartSlice.PieChartSliceBuilder(value).label(label).build());
+			break;
+		case BMI:
+			pieCharts[1].addPieChartSlice(new PieChartSlice.PieChartSliceBuilder(value).label(label).build());
+			break;
+		case bloodPressure:
+			pieCharts[2].addPieChartSlice(new PieChartSlice.PieChartSliceBuilder(value).label(label).build());
+			break;
+		default:
+			System.out.println("INVALID CHART TYPE");
+			break;
+		}
+	}
+	
+	public void printUrls()
+	{
+		System.out.println("Cholesterol:\n" + pieCharts[0].getUrl());
+		System.out.println("BMI:\n" + pieCharts[1].getUrl());
+		System.out.println("Blood Pressure:\n" + pieCharts[2].getUrl());
+	}
+	
+	public String getUrl(CHART_TYPE type)
+	{
+		switch(type)
+		{
+		case totalCholesterol:
+			return pieCharts[0].getUrl();
+		case BMI:
+			return pieCharts[1].getUrl();
+		case bloodPressure:
+			return pieCharts[2].getUrl();
+		default:
+			return "ERROR: Type does not exist, no URL available.";
+		}
+	}
+	
+	public boolean openWebPage()
+	{
+		return false;
+	}
+}
---
+++ assignment9/src/assignment9/PiechartWebServiceManagerTest.java
@@ -0,0 +1,58 @@
+package assignment9;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PiechartWebServiceManagerTest 
+{
+	private PiechartWebServiceManager pieChartManager = new PiechartWebServiceManager();
+
+	@Test
+	public void test_1() 
+	{
+		pieChartManager.addPieSlice(CHART_TYPE.totalCholesterol, "High", 18);
+		pieChartManager.addPieSlice(CHART_TYPE.totalCholesterol, "Borderline High", 14);
+		pieChartManager.addPieSlice(CHART_TYPE.totalCholesterol, "Desirable", 22);
+		
+		String chartUrl = pieChartManager.getUrl(CHART_TYPE.totalCholesterol);
+		
+		assertEquals("http://chart.apis.google.com/chart?cht=p&chs=500x250&chco=f032c8&chd=e:0Wou..&chl=High|Borderline High|Desirable",
+				chartUrl);
+		
+		System.out.println(chartUrl);
+	}
+	
+	@Test
+	public void test_2()
+	{
+		pieChartManager.addPieSlice(CHART_TYPE.BMI, "Overweight", 12);
+		pieChartManager.addPieSlice(CHART_TYPE.BMI, "Obese", 31);
+		pieChartManager.addPieSlice(CHART_TYPE.BMI, "Underweight", 17);
+		pieChartManager.addPieSlice(CHART_TYPE.BMI, "Normal", 26);
+		
+		String chartUrl = pieChartManager.getUrl(CHART_TYPE.BMI);
+
+		assertEquals("http://chart.apis.google.com/chart?cht=p&chs=500x250&chco=3232f0&chd=e:Yx..jG1r&chl=Overweight|Obese|Underweight|Normal",
+				pieChartManager.getUrl(CHART_TYPE.BMI));
+		
+		System.out.println(chartUrl);
+	}
+	
+	@Test
+	public void test_3()
+	{
+		pieChartManager.addPieSlice(CHART_TYPE.bloodPressure, "Stage 1 Hypertension", 17);
+		pieChartManager.addPieSlice(CHART_TYPE.bloodPressure, "Stage 2 Hypertension", 31);
+		pieChartManager.addPieSlice(CHART_TYPE.bloodPressure, "Hypertensive Crisis", 11);
+		pieChartManager.addPieSlice(CHART_TYPE.bloodPressure, "Normal", 54);
+		pieChartManager.addPieSlice(CHART_TYPE.bloodPressure, "Prehypertension", 23);
+
+		String chartUrl = pieChartManager.getUrl(CHART_TYPE.bloodPressure);
+		
+		assertEquals("http://chart.apis.google.com/chart?cht=p&chs=500x250&chco=b9000f&chd=e:UJkvNC..bQ&chl=Stage 1 Hypertension|Stage 2 Hypertension|Hypertensive Crisis|Normal|Prehypertension",
+				pieChartManager.getUrl(CHART_TYPE.bloodPressure));
+		
+		System.out.println(chartUrl);
+	}
+}
