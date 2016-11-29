2974f10627bb22062bd065452031412f6d37dc3a
CS360 Team 4 <elliae01@students.ipfw.edu>
2016-11-29 09:50:05 -0500

    Added initial model view controller packages

---
+++ assignment9/src/Model/PatientClassification.java
@@ -0,0 +1,66 @@
+package Model;
+
+public class PatientClassification {
+	String[]cholesterol;
+	String[]BMI;
+	String[]bloodpressure;
+	
+	public PatientClassification(){
+	cholesterol=new String[]{"DESIRABLE","BORDERLINE HIGH","HIGH"};
+	BMI=new String[]{"UNDERWEIGHT","NORMAL","OVERWEIGHT","OBESE"};
+	bloodpressure=new String[]{"NORMAL","PREHYPERTENSION","STAGE 1 HYPERTENSION",
+			"STAGE 2 HYPERTENSION","HYPERTENSIVE CRISIS"};
+	}
+
+	public String Cholesterol(int calc){
+		
+			if(calc<200&&calc>0){
+				return cholesterol[0];
+			}
+			if(calc>=200&&calc<=239){
+				return cholesterol[1];
+			}
+			if(calc>=240){
+				return cholesterol[2];
+			}else{
+				return "n/a";
+			}	
+	}
+	public String BMI(double calc){
+
+		if(calc<18.5&&calc>0){
+			return BMI[0];
+		}
+		if(calc>=18.5&&calc<=24.9){
+			return BMI[1];
+		}
+		if(calc>=25.0&&calc<=29.9){
+			return BMI[2];
+		}
+		if(calc>=30){
+			return BMI[3];
+		}else{
+			return "n/a";
+		}
+	}
+	public String bloodPressure(int sys,int dias){
+
+		if(sys<120&&sys>0&&dias<80&&dias>0){
+			return bloodpressure[0];
+		}
+		if(sys>=120&&sys<=139||dias>=80&&dias<=89){
+			return bloodpressure[1];
+		}
+		if(sys>=140&&sys<=159||dias>=90&&dias<=99){
+			return bloodpressure[2];
+		}
+		if(sys>=160&&sys<=180||dias>=100&&dias<=110){
+			return bloodpressure[3];
+		}
+		if(sys>=180&&sys<=110){
+			return bloodpressure[4];
+		}else{
+			return "n/a";
+		}
+	}
+}
---
+++ assignment9/src/View/PiechartUIinterface.java
@@ -0,0 +1,105 @@
+package View;
+
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import View.PiechartWebServiceManager;
+import View.CHART_TYPE;
+
+public class PiechartUIinterface  {
+
+	public PiechartUIinterface(){
+		
+	}
+	
+	public void chartExport(String[]data){
+		
+		String[]classifications={"DESIRABLE","BORDERLINE","HIGH","UNDERWEIGHT",
+				"N0RMAL","OVERWEIGHT","OBESE","NORMAL","PREHYPERTENSION",
+				"STAGE 1 HYPERTENSION","STAGE 2 HYPERTENSION", "HYPERTENSIVE CRISIS"};
+		int[]total = new int[12];
+		
+		for(int j=0;j<classifications.length;j++){
+			int k=0;
+			for(int i=0;i<data.length;i++){
+			  if (data[i].contains(classifications[j])){
+				   k=k+1;
+				   
+				  }
+			}
+			total[j]=k;
+		}
+		
+
+		PiechartWebServiceManager pc = new PiechartWebServiceManager();
+		
+		pc.addPieSlice(CHART_TYPE.totalCholesterol, "High", total[0]);
+		pc.addPieSlice(CHART_TYPE.totalCholesterol, "Borderline High", total[1]);
+		pc.addPieSlice(CHART_TYPE.totalCholesterol, "Desirable", total[2]);
+		String chartUrl = pc.getUrl(CHART_TYPE.totalCholesterol);
+		
+		pc.addPieSlice(CHART_TYPE.BMI, "Overweight", total[3]);
+		pc.addPieSlice(CHART_TYPE.BMI, "Obese", total[4]);
+		pc.addPieSlice(CHART_TYPE.BMI, "Underweight",total[5]);
+		pc.addPieSlice(CHART_TYPE.BMI, "Normal", total[6]);
+		String chartUrl2 = pc.getUrl(CHART_TYPE.BMI);
+		
+		pc.addPieSlice(CHART_TYPE.bloodPressure, "Stage 1 Hypertension",total[7]);
+		pc.addPieSlice(CHART_TYPE.bloodPressure, "Stage 2 Hypertension", total[8]);
+		pc.addPieSlice(CHART_TYPE.bloodPressure, "Hypertensive Crisis", total[9]);
+		pc.addPieSlice(CHART_TYPE.bloodPressure, "Normal", total[10]);
+		pc.addPieSlice(CHART_TYPE.bloodPressure, "Prehypertension", total[11]);
+		String chartUrl3 = pc.getUrl(CHART_TYPE.bloodPressure);
+		
+		System.out.println(chartUrl);
+		System.out.println(chartUrl2);
+		System.out.println(chartUrl3);
+		
+		URI uri = null;
+		URI uri2 = null;
+		URI uri3 = null;
+		
+	    try {
+		      URL url = new URL(chartUrl);
+		      URL url2 = new URL(chartUrl2);
+		      URL url3 = new URL(chartUrl3);
+		      String nullFragment = null;
+		      String nullFragment2 = null;
+		      String nullFragment3 = null;
+		      uri = new URI(url.getProtocol(), url.getHost(), url.getPath(), url.getQuery(), nullFragment);
+		      uri2 = new URI(url2.getProtocol(), url2.getHost(), url2.getPath(), url2.getQuery(), nullFragment2);
+		      uri3 = new URI(url3.getProtocol(), url3.getHost(), url3.getPath(), url3.getQuery(), nullFragment3);
+
+		      System.out.println("URI " + uri.toString() + " is OK");
+		      System.out.println("URI " + uri2.toString() + " is OK");
+		      System.out.println("URI " + uri3.toString() + " is OK");
+	    	} catch (MalformedURLException e) {
+			      System.out.println("URL " + chartUrl + " is a malformed URL");
+			      System.out.println("URL " + chartUrl2 + " is a malformed URL");
+			      System.out.println("URL " + chartUrl3 + " is a malformed URL");
+	    	} catch (URISyntaxException e) {
+			      System.out.println("URI " + chartUrl + " is a malformed URL");
+			      System.out.println("URL " + chartUrl2 + " is a malformed URL");
+			      System.out.println("URL " + chartUrl3 + " is a malformed URL");
+	   	}
+	  
+        Desktop d=Desktop.getDesktop();
+        // Browse a URL
+        
+        try {
+			d.browse(uri);
+			d.browse(uri2);
+			d.browse(uri3);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} 
+        
+    	
+	}
+
+}
