277229dfe07dfdf9e78bfb4e293a49cbe44a9ed0
CS360 Team 4 <elliae01@students.ipfw.edu>
2016-12-03 01:36:00 -0500

    editing

--- assignment9/src/View/PiechartWebServiceManager.java
+++ assignment9/src/View/PiechartWebServiceManager.java
@@ -2,7 +2,6 @@ package View;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import de.toolforge.googlechartwrapper.Color;
 import de.toolforge.googlechartwrapper.Dimension;
 import de.toolforge.googlechartwrapper.PieChart;
