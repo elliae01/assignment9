081f5994b29b989a90746697229c7feebc8e1f54
CS360 Team 4 <elliae01@students.ipfw.edu>
2016-12-04 06:27:49 -0500

    comment

--- assignment9/src/Controller/HealthScreening.java
+++ assignment9/src/Controller/HealthScreening.java
@@ -51,7 +51,7 @@ public class HealthScreening {
 
 
 	/**
-	 * Launch the application...
+	 * Launch the application.
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
