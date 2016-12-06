62006710f204c90215ed70eeb600200b81ac4d6a
Josh Hoffman <hoffja08@students.ipfw.edu>
2016-12-05 22:30:04 -0500

    Added more test cases for PatientSummary

--- assignment9/test/test/PatientSummaryTester.java
+++ assignment9/test/test/PatientSummaryTester.java
@@ -8,11 +8,35 @@ import View.PatientSummary;
 public class PatientSummaryTester {
 
 	@Test
-	public void test() {
+	public void test_high() {
 		PatientSummary ps = new PatientSummary();
 		
-		String testData[] = {"HIGH", "HIGH", "HIGH"};
-		assertEquals(ps.summary(testData), 3);
+		String testData[] = {"HIGH", "", ""};
+		assertEquals(ps.summary(testData), 1);
+	}
+
+	@Test
+	public void test_everything() {
+		PatientSummary ps = new PatientSummary();
+		
+		String testEverything[] = {"DESIRABLE","BORDERLINE","HIGH"};
+		assertEquals(ps.summary(testEverything), 3);
+	}
+
+	@Test
+	public void test_nothing() {
+		PatientSummary ps = new PatientSummary();
+		
+		String testData[] = {"", "", ""};
+		assertEquals(ps.summary(testData), 0);
+	}
+
+	@Test
+	public void test_invalid() {
+		PatientSummary ps = new PatientSummary();
+		
+		String testData[] = {"ZACK", "CODY", "MOSEBY"};
+		assertEquals(ps.summary(testData), 0);
 	}
 
 }
